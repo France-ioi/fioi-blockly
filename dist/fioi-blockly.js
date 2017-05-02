037d242203896011fdf2f59c4ef5367162f1fb4e
Michel Blockelet <blockelet@crans.org>
2017-05-02 17:58:24 +0200

    Fix procedure calls being renamed if they use the default name

--- dist/fioi-blockly.js
+++ dist/fioi-blockly.js
@@ -821,6 +821,14 @@ Blockly.WorkspaceSvg.prototype.reportValue = function(id, value) {
   Blockly.DropDownDiv.showPositionedByBlock(this, block);
 };
 
+Blockly.Xml.domToWorkspaceOriginal = Blockly.Xml.domToWorkspace;
+
+Blockly.Xml.domToWorkspace = function(xml, workspace) {
+  Blockly.disableRenameEvents = true;
+  Blockly.Xml.domToWorkspaceOriginal(xml, workspace);
+  Blockly.disableRenameEvents = false;
+}
+
 Blockly.Msg.VARIABLES_DEFAULT_NAME = "element";
 Blockly.Msg.TEXT_APPEND_VARIABLE = Blockly.Msg.VARIABLES_DEFAULT_NAME;
 
@@ -1387,6 +1395,35 @@ Blockly.Blocks['logic_compare'] = {
   }
 };
 
+
+Blockly.Blocks['procedures_callnoreturn'].renameProcedure = function(oldName, newName, force) {
+  if (!force && Blockly.disableRenameEvents) { return; }
+  if (Blockly.Names.equals(oldName, this.getProcedureCall())) {
+    this.setFieldValue(newName, 'NAME');
+    this.setTooltip(
+        (this.outputConnection ? Blockly.Msg.PROCEDURES_CALLRETURN_TOOLTIP :
+         Blockly.Msg.PROCEDURES_CALLNORETURN_TOOLTIP)
+        .replace('%1', newName));
+  }
+};
+
+Blockly.Blocks['procedures_callnoreturn'].domToMutation = function(xmlElement) {
+  var name = xmlElement.getAttribute('name');
+  this.renameProcedure(this.getProcedureCall(), name, true);
+  var args = [];
+  var paramIds = [];
+  for (var i = 0, childNode; childNode = xmlElement.childNodes[i]; i++) {
+    if (childNode.nodeName.toLowerCase() == 'arg') {
+      args.push(childNode.getAttribute('name'));
+      paramIds.push(childNode.getAttribute('paramId'));
+    }
+  }
+  this.setProcedureParameters_(args, paramIds);
+};
+
+
+Blockly.Blocks['procedures_callreturn'].renameProcedure = Blockly.Blocks['procedures_callnoreturn'].renameProcedure;
+Blockly.Blocks['procedures_callreturn'].domToMutation = Blockly.Blocks['procedures_callnoreturn'].domToMutation;
 
 Blockly.Blocks['text_print_noend'] = {
   /**
