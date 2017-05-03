771d79481401694518b53fd7f3f942e7d5a38d30
Michel Blockelet <blockelet@crans.org>
2017-05-03 20:05:34 +0200

    Fix some more issues

--- dist/fioi-blockly.js
+++ dist/fioi-blockly.js
@@ -574,6 +574,32 @@ Blockly.Scrollbar.prototype.resizeContentHorizontal = function(hostMetrics) {
 };
 
 
+// Fix case where there's no workspace (we're probably unloading)
+Blockly.onMouseUp_ = function(e) {
+  var workspace = Blockly.getMainWorkspace();
+  if (!workspace || workspace.dragMode_ == Blockly.DRAG_NONE) {
+    return;
+  }
+  Blockly.Touch.clearTouchIdentifier();
+  Blockly.Css.setCursor(Blockly.Css.Cursor.OPEN);
+  workspace.dragMode_ = Blockly.DRAG_NONE;
+  // Unbind the touch event if it exists.
+  if (Blockly.Touch.onTouchUpWrapper_) {
+    Blockly.unbindEvent_(Blockly.Touch.onTouchUpWrapper_);
+    Blockly.Touch.onTouchUpWrapper_ = null;
+  }
+  if (Blockly.onMouseMoveWrapper_) {
+    Blockly.unbindEvent_(Blockly.onMouseMoveWrapper_);
+    Blockly.onMouseMoveWrapper_ = null;
+  }
+};
+
+
+// Remove some characters which make JavaScript.STATEMENT_PREFIX instructions
+// generation go wrong
+Blockly.genUid.soup_ = '!#()*+,-./:;=?@[]_`{|}~' +
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
 // Options for the variables flyout
 Blockly.Variables.flyoutOptions = {
   any: true, // Allow to create any variable
