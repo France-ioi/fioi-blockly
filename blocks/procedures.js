a81ebd5f40d633318b7f263908ec501eb2ae4441
Michel Blockelet <blockelet@crans.org>
2017-05-11 13:56:44 +0200

    Can't overwrite init if it doesn't exist

--- blocks/procedures.js
+++ blocks/procedures.js
@@ -1,49 +1,52 @@
-Blockly.Blocks['procedures_defnoreturn'].init = function() {
-  var nameField = new Blockly.FieldTextInput('',
-      Blockly.Procedures.rename);
-  nameField.setSpellcheck(false);
-  this.appendDummyInput()
-      .appendField(Blockly.Msg.PROCEDURES_DEFNORETURN_TITLE)
-      .appendField(nameField, 'NAME')
-      .appendField('', 'PARAMS');
-  this.setMutator(new Blockly.Mutator(['procedures_mutatorarg']));
-  if ((this.workspace.options.comments ||
-       (this.workspace.options.parentWorkspace &&
-        this.workspace.options.parentWorkspace.options.comments)) &&
-      Blockly.Msg.PROCEDURES_DEFNORETURN_COMMENT) {
-    this.setCommentText(Blockly.Msg.PROCEDURES_DEFNORETURN_COMMENT);
-  }
-  this.setColour(Blockly.Blocks.procedures.HUE);
-  this.setTooltip(Blockly.Msg.PROCEDURES_DEFNORETURN_TOOLTIP);
-  this.setHelpUrl(Blockly.Msg.PROCEDURES_DEFNORETURN_HELPURL);
-  this.arguments_ = [];
-  this.setStatements_(true);
-  this.statementConnection_ = null;
-};
+if(Blockly.Blocks['procedures_defnoreturn']) {
+  Blockly.Blocks['procedures_defnoreturn'].init = function() {
+    var nameField = new Blockly.FieldTextInput('',
+        Blockly.Procedures.rename);
+    nameField.setSpellcheck(false);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.PROCEDURES_DEFNORETURN_TITLE)
+        .appendField(nameField, 'NAME')
+        .appendField('', 'PARAMS');
+    this.setMutator(new Blockly.Mutator(['procedures_mutatorarg']));
+    if ((this.workspace.options.comments ||
+         (this.workspace.options.parentWorkspace &&
+          this.workspace.options.parentWorkspace.options.comments)) &&
+        Blockly.Msg.PROCEDURES_DEFNORETURN_COMMENT) {
+      this.setCommentText(Blockly.Msg.PROCEDURES_DEFNORETURN_COMMENT);
+    }
+    this.setColour(Blockly.Blocks.procedures.HUE);
+    this.setTooltip(Blockly.Msg.PROCEDURES_DEFNORETURN_TOOLTIP);
+    this.setHelpUrl(Blockly.Msg.PROCEDURES_DEFNORETURN_HELPURL);
+    this.arguments_ = [];
+    this.setStatements_(true);
+    this.statementConnection_ = null;
+  };
+}
 
-
-Blockly.Blocks['procedures_defreturn'].init = function() {
-  var nameField = new Blockly.FieldTextInput('',
-      Blockly.Procedures.rename);
-  nameField.setSpellcheck(false);
-  this.appendDummyInput()
-      .appendField(Blockly.Msg.PROCEDURES_DEFRETURN_TITLE)
-      .appendField(nameField, 'NAME')
-      .appendField('', 'PARAMS');
-  this.appendValueInput('RETURN')
-      .setAlign(Blockly.ALIGN_RIGHT)
-      .appendField(Blockly.Msg.PROCEDURES_DEFRETURN_RETURN);
-  this.setMutator(new Blockly.Mutator(['procedures_mutatorarg']));
-  if ((this.workspace.options.comments ||
-       (this.workspace.options.parentWorkspace &&
-        this.workspace.options.parentWorkspace.options.comments)) &&
-      Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT) {
-    this.setCommentText(Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT);
-  }
-  this.setColour(Blockly.Blocks.procedures.HUE);
-  this.setTooltip(Blockly.Msg.PROCEDURES_DEFRETURN_TOOLTIP);
-  this.setHelpUrl(Blockly.Msg.PROCEDURES_DEFRETURN_HELPURL);
-  this.arguments_ = [];
-  this.setStatements_(true);
-  this.statementConnection_ = null;
-};
+if(Blockly.Blocks['procedures_defreturn']) {
+  Blockly.Blocks['procedures_defreturn'].init = function() {
+    var nameField = new Blockly.FieldTextInput('',
+        Blockly.Procedures.rename);
+    nameField.setSpellcheck(false);
+    this.appendDummyInput()
+        .appendField(Blockly.Msg.PROCEDURES_DEFRETURN_TITLE)
+        .appendField(nameField, 'NAME')
+        .appendField('', 'PARAMS');
+    this.appendValueInput('RETURN')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField(Blockly.Msg.PROCEDURES_DEFRETURN_RETURN);
+    this.setMutator(new Blockly.Mutator(['procedures_mutatorarg']));
+    if ((this.workspace.options.comments ||
+         (this.workspace.options.parentWorkspace &&
+          this.workspace.options.parentWorkspace.options.comments)) &&
+        Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT) {
+      this.setCommentText(Blockly.Msg.PROCEDURES_DEFRETURN_COMMENT);
+    }
+    this.setColour(Blockly.Blocks.procedures.HUE);
+    this.setTooltip(Blockly.Msg.PROCEDURES_DEFRETURN_TOOLTIP);
+    this.setHelpUrl(Blockly.Msg.PROCEDURES_DEFRETURN_HELPURL);
+    this.arguments_ = [];
+    this.setStatements_(true);
+    this.statementConnection_ = null;
+  };
+}
