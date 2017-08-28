382cc70d54aff63ab1cd9d10b0f620856907fbd9
Michel Blockelet <blockelet@crans.org>
2017-08-28 20:13:22 +0200

    Add english translations

---
+++ msg/js/en.js
@@ -0,0 +1,61 @@
+FioiBlockly.Msg.en = {};
+
+FioiBlockly.Msg.en.VARIABLES_DEFAULT_NAME = "element";
+FioiBlockly.Msg.en.TEXT_APPEND_VARIABLE = Blockly.Msg.VARIABLES_DEFAULT_NAME;
+
+FioiBlockly.Msg.en.DICTS_CREATE_EMPTY_TITLE = "empty dictionary";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_CONTAINER_TITLE_ADD = "Create a dictionary";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_CONTAINER_TOOLTIP = "";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_INPUT_WITH = "create a dictionary :";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_ITEM_KEY = "ckey";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_ITEM_MAPPING = ":";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_ITEM_TITLE = "key/value";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_ITEM_TOOLTIP = "";
+FioiBlockly.Msg.en.DICTS_CREATE_WITH_TOOLTIP = "";
+FioiBlockly.Msg.en.DICT_GET = "get the key";
+FioiBlockly.Msg.en.DICT_GET_TO = "from";
+FioiBlockly.Msg.en.DICT_KEYS = "list of the keys of";
+FioiBlockly.Msg.en.DICT_SET_TITLE = "assign to key";
+FioiBlockly.Msg.en.DICT_SET_OF = "of dictionary";
+FioiBlockly.Msg.en.DICT_SET_TO = "to";
+
+FioiBlockly.Msg.en.TEXT_PRINT_TITLE = "print line %1";
+FioiBlockly.Msg.en.TEXT_PRINT_TOOLTIP = "Print the text, number or other value, with a newline after.";
+FioiBlockly.Msg.en.TEXT_PRINT_NOEND_TITLE = "print %1";
+FioiBlockly.Msg.en.TEXT_PRINT_NOEND_TOOLTIP = "Print the text, number or other value, without newline.";
+
+FioiBlockly.Msg.en.LISTS_APPEND_MSG = "to the list %1 add the element %2";
+FioiBlockly.Msg.en.LISTS_APPEND_TOOLTIP = "Add an element to the list '%1'";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_FIRST = "at the beginning";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_FROM_END = "at the index from the end";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_FROM_START = "at the index";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_GET = "get value";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_GET_REMOVE = "get and remove value";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_LAST = "at the end";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_RANDOM = "at a random index";
+FioiBlockly.Msg.en.LISTS_GET_INDEX_REMOVE = "remove value";
+FioiBlockly.Msg.en.LISTS_SET_INDEX_INSERT = "insert";
+FioiBlockly.Msg.en.LISTS_SORT_TITLE = "return the sort %1 %2 of list %3"
+FioiBlockly.Msg.en.LISTS_SORT_PLACE_MSG = "sort list %1 in place";
+FioiBlockly.Msg.en.LISTS_SORT_PLACE_TOOLTIP = "Sorts list '%1' and modifies it directly.";
+
+FioiBlockly.Msg.en.INPUT_NUM = "read a single number on the whole line";
+FioiBlockly.Msg.en.INPUT_NUM_TOOLTIP = "Reads a single number on a line, on the program input.";
+FioiBlockly.Msg.en.INPUT_NUM_LIST = "read a list of numbers on a line";
+FioiBlockly.Msg.en.INPUT_NUM_LIST_TOOLTIP = "Reads a list of numbers on a line, on the program input.";
+FioiBlockly.Msg.en.INPUT_NUM_NEXT = "read a number";
+FioiBlockly.Msg.en.INPUT_NUM_NEXT_TOOLTIP = "Reads the next number on the program input.";
+FioiBlockly.Msg.en.INPUT_CHAR = "read a character";
+FioiBlockly.Msg.en.INPUT_CHAR_TOOLTIP = "Reads a character on the program input.";
+FioiBlockly.Msg.en.INPUT_WORD = "read a word";
+FioiBlockly.Msg.en.INPUT_WORD_TOOLTIP = "Reads a word on the program input.";
+FioiBlockly.Msg.en.INPUT_LINE = "read a line";
+FioiBlockly.Msg.en.INPUT_LINE_TOOLTIP = "Reads a line on the program input.";
+
+FioiBlockly.Msg.en.CANNOT_DELETE_VARIABLE_PROCEDURE = "Cannot delete variable '%1', used by procedure '%2'.";
+
+FioiBlockly.Msg.en.DATA_REPLACEITEMOFLIST_TITLE = "replace element %1 of list %2 with %3";
+FioiBlockly.Msg.en.DATA_ITEMOFLIST_TITLE = "element %1 in %2";
+FioiBlockly.Msg.en.DATA_LISTREPEAT_TITLE = "initialize list %1 with %2 repeated %3 times";
+
+FioiBlockly.Msg.en.INVALID_NAME = "Invalid name, please only use letters, some letters with accents, digits (except as first character), and underscore '_'.";
