71ed2ff0a058b933ce152681a4d47adc22d818b5
Humberto Sermeno <hsermeno@gmail.com>
2018-03-13 17:39:25 -0600

    Spanish translations

---
+++ msg/js/es.js
@@ -0,0 +1,61 @@
+FioiBlockly.Msg.es = {};
+
+FioiBlockly.Msg.es.VARIABLES_DEFAULT_NAME = "elemento";
+FioiBlockly.Msg.es.TEXT_APPEND_VARIABLE = Blockly.Msg.VARIABLES_DEFAULT_NAME;
+
+FioiBlockly.Msg.es.DICTS_CREATE_EMPTY_TITLE = "diccionario vacío";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_CONTAINER_TITLE_ADD = "Crear un diccionario";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_CONTAINER_TOOLTIP = "";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_INPUT_WITH = "crear un diccionario:";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_ITEM_KEY = "llave";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_ITEM_MAPPING = ":";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_ITEM_TITLE = "llave/valor";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_ITEM_TOOLTIP = "";
+FioiBlockly.Msg.es.DICTS_CREATE_WITH_TOOLTIP = "";
+FioiBlockly.Msg.es.DICT_GET = "recuperar la llave";
+FioiBlockly.Msg.es.DICT_GET_TO = "de";
+FioiBlockly.Msg.es.DICT_KEYS = "listado de llaves de";
+FioiBlockly.Msg.es.DICT_SET_TITLE = "modificar la llave";
+FioiBlockly.Msg.es.DICT_SET_OF = "del diccionario";
+FioiBlockly.Msg.es.DICT_SET_TO = "a";
+
+FioiBlockly.Msg.es.TEXT_PRINT_TITLE = "imprimir línea %1";
+FioiBlockly.Msg.es.TEXT_PRINT_TOOLTIP = "Imprimir el texto, número u otro valor, seguido de un retorno de línea.";
+FioiBlockly.Msg.es.TEXT_PRINT_NOEND_TITLE = "imprimir %1";
+FioiBlockly.Msg.es.TEXT_PRINT_NOEND_TOOLTIP = "Imprimir el texto, número u otro valor, sin retorno de línea.";
+
+FioiBlockly.Msg.es.LISTS_APPEND_MSG = "a la lista %1 agregar el elemento %2";
+FioiBlockly.Msg.es.LISTS_APPEND_TOOLTIP = "Agregar un elemento a la lista '%1'";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_FIRST = "al inicio";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_FROM_END = "en el índice a partir del final";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_FROM_START = "en el índice";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_GET = "obtener valor";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_GET_REMOVE = "obtener y remover valor";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_LAST = "al final";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_RANDOM = "en un índice aleatorio";
+FioiBlockly.Msg.es.LISTS_GET_INDEX_REMOVE = "remover el valor";
+FioiBlockly.Msg.es.LISTS_SET_INDEX_INSERT = "insertar";
+FioiBlockly.Msg.es.LISTS_SORT_TITLE = "retornar el ordenamiento %1 %2 de la lista %3"
+FioiBlockly.Msg.es.LISTS_SORT_PLACE_MSG = "ordenar la lista %1 en su lugar";
+FioiBlockly.Msg.es.LISTS_SORT_PLACE_TOOLTIP = "Ordena la lista '%1' y la modifica directamente.";
+
+FioiBlockly.Msg.es.INPUT_NUM = "leer un sólo número en toda la línea";
+FioiBlockly.Msg.es.INPUT_NUM_TOOLTIP = "Lee un sólo número en una lína, en la entrada del programa.";
+FioiBlockly.Msg.es.INPUT_NUM_LIST = "leer una lista de números en una línea";
+FioiBlockly.Msg.es.INPUT_NUM_LIST_TOOLTIP = "Lee una lista de números en una línea, en la entrada del programa.";
+FioiBlockly.Msg.es.INPUT_NUM_NEXT = "leer un número";
+FioiBlockly.Msg.es.INPUT_NUM_NEXT_TOOLTIP = "Leer el próximo número en la entrada del programa.";
+FioiBlockly.Msg.es.INPUT_CHAR = "leer un caracter";
+FioiBlockly.Msg.es.INPUT_CHAR_TOOLTIP = "Lee un caracter desde la entrada del programa.";
+FioiBlockly.Msg.es.INPUT_WORD = "leer una palabra";
+FioiBlockly.Msg.es.INPUT_WORD_TOOLTIP = "Lee una palabra desde la entrada del programa.";
+FioiBlockly.Msg.es.INPUT_LINE = "leer una línea";
+FioiBlockly.Msg.es.INPUT_LINE_TOOLTIP = "Lee una línea desde la entrada del programa.";
+
+FioiBlockly.Msg.es.CANNOT_DELETE_VARIABLE_PROCEDURE = "No es posible borrar la variable '%1', es utilizada en el procedimiento '%2'.";
+
+FioiBlockly.Msg.es.DATA_REPLACEITEMOFLIST_TITLE = "reemplazar el elemento %1 de la lista %2 con %3";
+FioiBlockly.Msg.es.DATA_ITEMOFLIST_TITLE = "elemento %1 en %2";
+FioiBlockly.Msg.es.DATA_LISTREPEAT_TITLE = "inicializar la lista %1 con %2 repetido %3 veces";
+
+FioiBlockly.Msg.es.INVALID_NAME = "Nombre inválido. Favor utilice únicamente letras, algunas letras acentuadas, dígitos (excepto como primer caracter), y guión bajo '_'.";
