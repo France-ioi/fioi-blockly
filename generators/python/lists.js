bf51fd7fa45e1adb987f19e503c3ad783425f6b9
Michel Blockelet <blockelet@crans.org>
2018-06-02 21:19:05 +0200

    Python: Allow to set any index of a list

--- generators/python/lists.js
+++ generators/python/lists.js
@@ -7,6 +7,86 @@ Blockly.Python['lists_append'] = function(block) {
   return varName + '.append(' + value + ')\n';
 };
 
+Blockly.Python['lists_setIndex'] = function(block) {
+  // Set element at index.
+  // Note: Until February 2013 this block did not have MODE or WHERE inputs.
+  var list = Blockly.Python.valueToCode(block, 'LIST',
+      Blockly.Python.ORDER_MEMBER) || '[]';
+  var mode = block.getFieldValue('MODE') || 'GET';
+  var where = block.getFieldValue('WHERE') || 'FROM_START';
+  var value = Blockly.Python.valueToCode(block, 'TO',
+      Blockly.Python.ORDER_NONE) || 'None';
+  // Cache non-trivial values to variables to prevent repeated look-ups.
+  // Closure, which accesses and modifies 'list'.
+  function cacheList() {
+    if (list.match(/^\w+$/)) {
+      return '';
+    }
+    var listVar = Blockly.Python.variableDB_.getDistinctName(
+        'tmp_list', Blockly.Variables.NAME_TYPE);
+    var code = listVar + ' = ' + list + '\n';
+    list = listVar;
+    return code;
+  }
+
+  if(mode == 'SET') {
+    Blockly.Python.definitions_['lists_assignIndex'] = '' +
+      'def assignIndex(l, i, x):\n' +
+      '    n = len(l)\n' +
+      '    if i >= n:\n' +
+      '        l.extend([None]*(i-n+1))\n' +
+      '    l[i] = x\n';
+  }
+
+  switch (where) {
+    case 'FIRST':
+      if (mode == 'SET') {
+        return 'assignIndex(' + list + ', 0, ' + value + ')\n';
+      } else if (mode == 'INSERT') {
+        return list + '.insert(0, ' + value + ')\n';
+      }
+      break;
+    case 'LAST':
+        if (mode == 'SET') {
+          return 'assignIndex(' + list + ', -1, ' + value + ')\n';
+        } else if (mode == 'INSERT') {
+          return list + '.append(' + value + ')\n';
+        }
+      break;
+    case 'FROM_START':
+      var at = Blockly.Python.getAdjustedInt(block, 'AT');
+        if (mode == 'SET') {
+          return 'assignIndex(' + list + ', ' + at + ', ' + value + ')\n';
+        } else if (mode == 'INSERT') {
+          return list + '.insert(' + at + ', ' + value + ')\n';
+        }
+      break;
+    case 'FROM_END':
+      var at = Blockly.Python.getAdjustedInt(block, 'AT', 1, true);
+        if (mode == 'SET') {
+          return 'assignIndex(' + list + ', ' + at + ', ' + value + ')\n';
+        } else if (mode == 'INSERT') {
+          return list + '.insert(' + at + ', ' + value + ')\n';
+        }
+      break;
+    case 'RANDOM':
+        Blockly.Python.definitions_['import_random'] = 'import random';
+        var code = cacheList();
+        var xVar = Blockly.Python.variableDB_.getDistinctName(
+            'tmp_x', Blockly.Variables.NAME_TYPE);
+        code += xVar + ' = int(random.random() * len(' + list + '))\n';
+        if (mode == 'SET') {
+          code += 'assignIndex(' + list + ', ' + xVar + ', ' + value + ')\n';
+          return code;
+        } else if (mode == 'INSERT') {
+          code += list + '.insert(' + xVar + ', ' + value + ')\n';
+          return code;
+        }
+      break;
+  }
+  throw 'Unhandled combination (lists_setIndex).';
+};
+
 Blockly.Python['lists_sort_place'] = function(block) {
   var varName = Blockly.Python.variableDB_.getName(block.getFieldValue('VAR'),
       Blockly.Variables.NAME_TYPE);
