21418eaf3b8ff1118116a863e5df117ae29c718e
Michel Blockelet <blockelet@crans.org>
2019-01-03 02:43:30 +0100

    Zoom from origin x = 0, y = 0

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -30,3 +30,37 @@ Blockly.WorkspaceSvg.prototype.translate = function(x, y) {
     this.dragSurface.translateAndScaleGroup(x, y, this.scale);
   }
 };
+
+// Change zoom origin to be x = 0, y = 0 instead of the center of the screen
+Blockly.WorkspaceSvg.prototype.zoom = function(x, y, type) {
+  var speed = this.options.zoomOptions.scaleSpeed;
+  var metrics = this.getMetrics();
+  var center = this.getParentSvg().createSVGPoint();
+  center.x = 0;
+  center.y = 0;
+  center = center.matrixTransform(this.getCanvas().getCTM().inverse());
+  x = center.x;
+  y = center.y;
+  var canvas = this.getCanvas();
+  // Scale factor.
+  var scaleChange = (type == 1) ? speed : 1 / speed;
+  // Clamp scale within valid range.
+  var newScale = this.scale * scaleChange;
+  if (newScale > this.options.zoomOptions.maxScale) {
+    scaleChange = this.options.zoomOptions.maxScale / this.scale;
+  } else if (newScale < this.options.zoomOptions.minScale) {
+    scaleChange = this.options.zoomOptions.minScale / this.scale;
+  }
+  if (this.scale == newScale) {
+    return;  // No change in zoom.
+  }
+  if (this.scrollbar) {
+    var matrix = canvas.getCTM()
+        .translate(x * (1 - scaleChange), y * (1 - scaleChange))
+        .scale(scaleChange);
+    // newScale and matrix.a should be identical (within a rounding error).
+    this.scrollX = matrix.e - metrics.absoluteLeft;
+    this.scrollY = matrix.f - metrics.absoluteTop;
+  }
+  this.setScale(newScale);
+};
