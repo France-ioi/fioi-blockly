21ae8b39c5edff3fbf933590d9f7f66c3fc1c342
Michel Blockelet <blockelet@crans.org>
2020-09-11 14:41:34 +0200

    msg: Add it translations

--- dist/fioi-blockly.js
+++ dist/fioi-blockly.js
@@ -1675,6 +1675,99 @@ FioiBlockly.Msg.fr.TABLES_OUT_OF_BOUNDS = "Tentative d'accès à une case hors d
 FioiBlockly.Msg.fr.MATH_DIVISIONFLOOR_SYMBOL = ' // ';
 FioiBlockly.Msg.fr.MATH_ARITHMETIC_TOOLTIP_DIVIDEFLOOR = "Renvoie la partie entière de la division des deux nombres.";
 
+FioiBlockly.Msg.it = {};
+
+FioiBlockly.Msg.it.VARIABLES_DEFAULT_NAME = "element";
+FioiBlockly.Msg.it.TEXT_APPEND_VARIABLE = Blockly.Msg.VARIABLES_DEFAULT_NAME;
+
+FioiBlockly.Msg.it.DICTS_CREATE_EMPTY_TITLE = "dictionnaire vide";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_CONTAINER_TITLE_ADD = "Crea un dizionario";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_CONTAINER_TOOLTIP = "";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_INPUT_WITH = "crea un dizionario :";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_ITEM_KEY = "chiave";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_ITEM_MAPPING = ":";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_ITEM_TITLE = "chiave/valore";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_ITEM_TOOLTIP = "";
+FioiBlockly.Msg.it.DICTS_CREATE_WITH_TOOLTIP = "";
+FioiBlockly.Msg.it.DICT_GET = "recupera la chiave";
+FioiBlockly.Msg.it.DICT_GET_TO = "di";
+FioiBlockly.Msg.it.DICT_KEYS = "elenco delle chiavi di";
+FioiBlockly.Msg.it.DICT_SET_TITLE = "assegna la chiave";
+FioiBlockly.Msg.it.DICT_SET_OF = "del dizionario";
+FioiBlockly.Msg.it.DICT_SET_TO = "a";
+
+FioiBlockly.Msg.it.TEXT_PRINT_TITLE = "mostra la riga %1";
+FioiBlockly.Msg.it.TEXT_PRINT_TOOLTIP = "Mostra il testo, numero o altro valore specificato, con interruzione di riga dopo.";
+FioiBlockly.Msg.it.TEXT_PRINT_NOEND_TITLE = "mostra %1";
+FioiBlockly.Msg.it.TEXT_PRINT_NOEND_TOOLTIP = "Mostra il testo, il numero o altro valore specificato, senza interruzioni di riga.";
+
+FioiBlockly.Msg.it.TEXT_EVAL_TITLE = "valuta";
+FioiBlockly.Msg.it.TEXT_EVAL_TOOLTIP = "Valuta l'espressione aritmetica specificata.";
+FioiBlockly.Msg.it.TEXT_EVAL_INVALID = "Attenzione : %1 ; questo blocco sarà \"falso\" !";
+
+FioiBlockly.Msg.it.EVAL_ERROR_SEMICOLON = "il punto e virgola ';' non è autorizzato";
+FioiBlockly.Msg.it.EVAL_ERROR_SYNTAX = "l'espressione non è sintatticamente valida";
+FioiBlockly.Msg.it.EVAL_ERROR_TYPE = "questo tipo di espressione (%1) non è autorizzato";
+FioiBlockly.Msg.it.EVAL_ERROR_VAR = "questa espressione utilizza una variabile '%1' non definita";
+
+FioiBlockly.Msg.it.LISTS_APPEND_MSG = "all'elenco %1 aggiungi l'elemento %2";
+FioiBlockly.Msg.it.LISTS_APPEND_TOOLTIP = "Aggiungi un elemento all'elenco '%1'";
+FioiBlockly.Msg.it.LISTS_CREATE_WITH_TOO_LARGE = "Dimensione della lista troppo grande: %1 > dimensione massima consentita %2"
+FioiBlockly.Msg.it.LISTS_GET_INDEX_FIRST = "all'inizio";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_FROM_END = "sull'indice dalla fine";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_FROM_START = "all'indice";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_GET = "ottieni il valore";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_GET_REMOVE = "ottieni e rimuovi il valore";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_LAST = "alla fine";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_RANDOM = "su un indice aleatorio";
+FioiBlockly.Msg.it.LISTS_GET_INDEX_REMOVE = "rimuovi il valore";
+FioiBlockly.Msg.it.LISTS_SET_INDEX_INSERT = "inserisci";
+FioiBlockly.Msg.it.LISTS_SORT_TITLE = "sposta la selezione %1 %2 dell'elenco %3"
+FioiBlockly.Msg.it.LISTS_SORT_PLACE_MSG = "seleziona l'elenco %1 sul posto";
+FioiBlockly.Msg.it.LISTS_SORT_PLACE_TOOLTIP = "Seleziona la lista '%1' e modificala istantaneamentela.";
+
+FioiBlockly.Msg.it.INPUT_NUM = "leggi un nome solo su una riga";
+FioiBlockly.Msg.it.INPUT_NUM_TOOLTIP = "Leggi un numero solo su una riga, sull'input del programma.";
+FioiBlockly.Msg.it.INPUT_NUM_LIST = "leggi un elenco di numeri su una riga";
+FioiBlockly.Msg.it.INPUT_NUM_LIST_TOOLTIP = "Leggi un elenco di numeri sull'elenco, sull'input del programma.";
+FioiBlockly.Msg.it.INPUT_NUM_NEXT = "leggi un numero";
+FioiBlockly.Msg.it.INPUT_NUM_NEXT_TOOLTIP = "Leggi il prossimo numero sull'input del programma.";
+FioiBlockly.Msg.it.INPUT_CHAR = "leggi un carattere";
+FioiBlockly.Msg.it.INPUT_CHAR_TOOLTIP = "Leggi un carattere sull'input del programma.";
+FioiBlockly.Msg.it.INPUT_WORD = "leggi una parola";
+FioiBlockly.Msg.it.INPUT_WORD_TOOLTIP = "Leggi una parola sull'input del programma.";
+FioiBlockly.Msg.it.INPUT_LINE = "leggi una riga";
+FioiBlockly.Msg.it.INPUT_LINE_TOOLTIP = "Leggi una riga sull'input del programma.";
+
+FioiBlockly.Msg.it.CANNOT_DELETE_VARIABLE_PROCEDURE = "Impossibile rimuovere la variabile '%1', utilizzata dalla procedura '%2'.";
+
+FioiBlockly.Msg.it.DATA_REPLACEITEMOFLIST_TITLE = "rimpiazza l'elemento %1 dell'elenco %2 con %3";
+FioiBlockly.Msg.it.DATA_ITEMOFLIST_TITLE = "elemento %1 in %2";
+FioiBlockly.Msg.it.DATA_LISTREPEAT_TITLE = "inizializza l'elenco %1 con %2 ripetuto %3 volte";
+
+FioiBlockly.Msg.it.INVALID_NAME = "Nome non valido, si prega di utilizzare solo lettere, lettere accentate in francese, numeri (eccetto come primo carattere) e trattini bassi '_'.";
+
+FioiBlockly.Msg.it.TABLES_2D_INIT = "inizializza la tabella 2D %1 con %2 linee e %3 colonne contenenti %4";
+FioiBlockly.Msg.it.TABLES_2D_INIT_TOOLTIP = "Crea una tabella con il numero di righe e colonne specificato e inizializza ogni cella al valore dato.";
+FioiBlockly.Msg.it.TABLES_2D_SET = "in %1[%2] [%3] inserisci %4";
+FioiBlockly.Msg.it.TABLES_3D_SET_TOOLTIP = "Inserisci il valore nella casella [riga] [colonna] della tabella %1.";
+FioiBlockly.Msg.it.TABLES_2D_GET = "%1[%2] [%3]";
+FioiBlockly.Msg.it.TABLES_2D_GET_TOOLTIP = "Recupera il valore dalla casella [riga] [colonna] della tabella %1.";
+
+FioiBlockly.Msg.it.TABLES_3D_INIT = "inizializza la tabella 3D %1 con %2 livelli, %3 linee, %4 colonne contenenti %5";
+FioiBlockly.Msg.it.TABLES_3D_INIT_TOOLTIP = "Crea una tabella con il numero specificato di righe, colonne e livelli, e inizializza ogni cella al valore dato.";
+FioiBlockly.Msg.it.TABLES_3D_SET = "in %1[%2] [%3] [%4] inserisci %5";
+FioiBlockly.Msg.it.TABLES_3D_SET_TOOLTIP = "Inserisci il valore nella casella [livello] [riga] [colonna] della tabella %1.";
+FioiBlockly.Msg.it.TABLES_3D_GET = "%1[%2] [%3] [%4]";
+FioiBlockly.Msg.it.TABLES_3D_GET_TOOLTIP = "Recupera il valore nella casella [livello] [riga] [colonna] della tabella %1.";
+
+FioiBlockly.Msg.it.TABLES_VAR_NAME = "tabella";
+FioiBlockly.Msg.it.TABLES_TOO_BIG = "Dimensioni della tabella troppo grandi !";
+FioiBlockly.Msg.it.TABLES_OUT_OF_BOUNDS = "Tentativo d'accesso a una casella fuori da una tabella !";
+
+FioiBlockly.Msg.it.MATH_DIVISIONFLOOR_SYMBOL = ' // ';
+FioiBlockly.Msg.it.MATH_ARITHMETIC_TOOLTIP_DIVIDEFLOOR = "Riporta la parte intera della divisione dei due numeri.";
+
 FioiBlockly.Msg.sl = {};
 
 FioiBlockly.Msg.sl.VARIABLES_DEFAULT_NAME = "element";
@@ -2362,6 +2455,59 @@ Blockly.Blocks['logic_compare'] = {
   }
 };
 
+
+Blockly.Blocks['math_arithmetic'] = {
+  /**
+   * Block for basic arithmetic operator.
+   * @this Blockly.Block
+   */
+  init: function() {
+    this.jsonInit({
+      "message0": "%1 %2 %3",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "A",
+          "check": "Number"
+        },
+        {
+          "type": "field_dropdown",
+          "name": "OP",
+          "options":
+            [[Blockly.Msg.MATH_ADDITION_SYMBOL, 'ADD'],
+             [Blockly.Msg.MATH_SUBTRACTION_SYMBOL, 'MINUS'],
+             [Blockly.Msg.MATH_MULTIPLICATION_SYMBOL, 'MULTIPLY'],
+             [Blockly.Msg.MATH_DIVISION_SYMBOL, 'DIVIDE'],
+             [Blockly.Msg.MATH_DIVISIONFLOOR_SYMBOL, 'DIVIDEFLOOR'],
+             [Blockly.Msg.MATH_POWER_SYMBOL, 'POWER']]
+        },
+        {
+          "type": "input_value",
+          "name": "B",
+          "check": "Number"
+        }
+      ],
+      "inputsInline": true,
+      "output": "Number",
+      "colour": Blockly.Blocks.math.HUE,
+      "helpUrl": Blockly.Msg.MATH_ARITHMETIC_HELPURL
+    });
+    // Assign 'this' to a variable for use in the tooltip closure below.
+    var thisBlock = this;
+    this.setTooltip(function() {
+      var mode = thisBlock.getFieldValue('OP');
+      var TOOLTIPS = {
+        'ADD': Blockly.Msg.MATH_ARITHMETIC_TOOLTIP_ADD,
+        'MINUS': Blockly.Msg.MATH_ARITHMETIC_TOOLTIP_MINUS,
+        'MULTIPLY': Blockly.Msg.MATH_ARITHMETIC_TOOLTIP_MULTIPLY,
+        'DIVIDE': Blockly.Msg.MATH_ARITHMETIC_TOOLTIP_DIVIDE,
+        'DIVIDEFLOOR': Blockly.Msg.MATH_ARITHMETIC_TOOLTIP_DIVIDEFLOOR,
+        'POWER': Blockly.Msg.MATH_ARITHMETIC_TOOLTIP_POWER
+      };
+      return TOOLTIPS[mode];
+    });
+  }
+};
 
 if(Blockly.Blocks['procedures_defnoreturn']) {
   Blockly.Blocks['procedures_defnoreturn'].init = function() {
@@ -3766,6 +3912,31 @@ Blockly.Python['controls_repeat_ext'] = function(block) {
 };
 
 Blockly.Python['controls_repeat'] = Blockly.Python['controls_repeat_ext'];
+
+Blockly.Python['math_arithmetic'] = function(block) {
+  // Basic arithmetic operators, and power.
+  var OPERATORS = {
+    'ADD': [' + ', Blockly.Python.ORDER_ADDITIVE],
+    'MINUS': [' - ', Blockly.Python.ORDER_ADDITIVE],
+    'MULTIPLY': [' * ', Blockly.Python.ORDER_MULTIPLICATIVE],
+    'DIVIDE': [' / ', Blockly.Python.ORDER_MULTIPLICATIVE],
+    'DIVIDEFLOOR': [' // ', Blockly.Python.ORDER_MULTIPLICATIVE],
+    'POWER': [' ** ', Blockly.Python.ORDER_EXPONENTIATION]
+  };
+  var tuple = OPERATORS[block.getFieldValue('OP')];
+  var operator = tuple[0];
+  var order = tuple[1];
+  var argument0 = Blockly.Python.valueToCode(block, 'A', order) || '0';
+  var argument1 = Blockly.Python.valueToCode(block, 'B', order) || '0';
+  var code = argument0 + operator + argument1;
+  return [code, order];
+  // In case of 'DIVIDE', division between integers returns different results
+  // in Python 2 and 3. However, is not an issue since Blockly does not
+  // guarantee identical results in all languages.  To do otherwise would
+  // require every operator to be wrapped in a function call.  This would kill
+  // legibility of the generated code.
+};
+
 
 Blockly.Python['tables_2d_init'] = function(block) {
   var blockVarName = block.getFieldValue('VAR');
